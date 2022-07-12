b475646f7255bf7351a33ef0d370ae72143587eb
kristallsi <107290487+kristallsi@users.noreply.github.com>
2022-07-12 09:23:54 +0800

    Update nfhk.js

--- nfhk.js
+++ nfhk.js
@@ -23,7 +23,7 @@ const $ = new Env("南方航空");
 ///////////////////////////////////////////////////////////////////
 const notify = $.isNode() ? require("./sendNotify") : "";
 const Notify = 1 		//0为关闭通知,1为打开通知,默认为1
-const debug = 1			//0为关闭调试,1为打开调试,默认为0
+const debug = 0			//0为关闭调试,1为打开调试,默认为0
 ///////////////////////////////////////////////////////////////////
 let ckStr = process.env.nfhk_Cookie;
 let msg = "";
